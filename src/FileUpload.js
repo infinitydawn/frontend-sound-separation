78342fee3ec8afcdc5b653d81f7b68bb2008d313
Mikhail Nikulin <nklnmish@gmail.com>
2024-11-12 23:44:34 -0500

    deploy 2

--- src/FileUpload.js
+++ src/FileUpload.js
@@ -12,6 +12,8 @@ function FileUpload({ onSelectFile, onUpdateData, onFileID }) {
   const [sampleRate, setSampleRate] = useState(22050);
   const [windowSize, setWinSize] = useState(1024);
   const [hopLength, setHopLength] = useState(512);
+  const [horizFilter, setHorizFilt] = useState(23);
+  const [vertFilter, setVertFilt] = useState(9);
 
   // handlers for states changes
   const handleFileChange = (event) => {
@@ -28,6 +30,12 @@ function FileUpload({ onSelectFile, onUpdateData, onFileID }) {
   const handleHopLengthChange = (event) => {
     setHopLength(event.target.value);
   };
+  const handleHorizFilterChange = (event) => {
+    setHorizFilt(event.target.value);
+  };
+  const handleVertFilterChange = (event) => {
+    setVertFilt(event.target.value);
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -41,10 +49,10 @@ function FileUpload({ onSelectFile, onUpdateData, onFileID }) {
         const params = JSON.stringify({
           sampleRate: sampleRate,
           windowSize: windowSize,
-          hopLength: hopLength
+          hopLength: hopLength,
+          horizFilter: horizFilter,
+          vertFilter: vertFilter
         })
-
-        alert(params)
 
         formData.append('file', selectedFile);
         formData.append('params', params);
@@ -105,9 +113,16 @@ function FileUpload({ onSelectFile, onUpdateData, onFileID }) {
           <label htmlFor='hop-length'>Hop/Stride Length</label>
           <input id='hop-length' type="number" onChange={handleHopLengthChange} placeholder='Set hop/stride length' value={ hopLength }/>
 
+          <label htmlFor='horiz-filter'>Amount Of Horizontal Median Filtering (1 for none)</label>
+          <input id='horiz-filter' type="number" onChange={handleHorizFilterChange} placeholder='Set Horizontal Filter' value={ horizFilter }/>
+
+          <label htmlFor='vert-filter'>Amount Of Vertical Median Filtering (1 for none)</label>
+          <input id='vert-filter' type="number" onChange={handleVertFilterChange} placeholder='Set Vertical Filter' value={ vertFilter }/>
+
         </div>
 
         <button type="submit">Upload</button>
+        <h4>*Note: for every new request please reload the page! (I will fix it soon)</h4>
       </form>
     </div>
   );
